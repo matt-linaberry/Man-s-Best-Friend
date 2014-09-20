3815f0c65aea5bd2c2f588ed8b1d6f095681e0e5
Matt Linaberry <matt.linaberry@we-ics.com>
2014-09-20 11:46:34 -0400

    Up through the random dog challenge!

--- Man's Best Friend/MTLViewController.h
+++ Man's Best Friend/MTLViewController.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface MTLViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *myImageView;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *breedLabel;
+@property (strong, nonatomic) NSMutableArray *myDogs;
+@property (nonatomic) int lastDogSelected;
+
+- (IBAction)newDogBarButtonItemPressed:(UIBarButtonItem *)sender;
 - (void) printHelloWorld;
 @end
--- Man's Best Friend/MTLViewController.m
+++ Man's Best Friend/MTLViewController.m
@@ -22,15 +22,63 @@
     myDog.name = @"Fido";
     myDog.breed = @"St. Brenard";
     myDog.age = 3;
+    myDog.image = [UIImage imageNamed:@"St.Bernard.JPG"];
     
-    int dogYears = [myDog ageInDogYearsFromAge:myDog.age];
-    NSLog(@"Dog years for %@ is %i", myDog.name, dogYears);
+    MTLDog *secondDog = [[MTLDog alloc] init];
+    secondDog.name = @"Wishbone";
+    secondDog.breed = @"Jack Russell Terrier";
+    secondDog.image = [UIImage imageNamed:@"JRT.jpg"];
+    
+    MTLDog *thirdDog = [[MTLDog alloc] init];
+    thirdDog.name = @"Lassie";
+    thirdDog.breed = @"Collie";
+    thirdDog.image = [UIImage imageNamed:@"BorderCollie.jpg"];
+    
+    MTLDog *fourthDog = [[MTLDog alloc] init];
+    fourthDog.name = @"Angel";
+    fourthDog.breed = @"Greyhound";
+    fourthDog.image = [UIImage imageNamed:@"ItalianGreyhound.jpg"];
+    
+    self.myDogs = [[NSMutableArray alloc] init];
+    [self.myDogs addObject:myDog];
+    [self.myDogs addObject:secondDog];
+    [self.myDogs addObject:thirdDog];
+    [self.myDogs addObject:fourthDog];
+    
+    self.myImageView.image = myDog.image;
+    self.nameLabel.text = myDog.name;
+    self.breedLabel.text = myDog.breed;
+    self.lastDogSelected = 0;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)newDogBarButtonItemPressed:(UIBarButtonItem *)sender
+{
+    int numberOfDogs = [self.myDogs count];
+    int randomIndex = arc4random() % numberOfDogs;
+
+    while (self.lastDogSelected == randomIndex)
+    {
+        randomIndex = arc4random() % numberOfDogs;
+    }
+    self.lastDogSelected = randomIndex;
+    MTLDog *randomDog = [self.myDogs objectAtIndex:randomIndex];
+//    self.myImageView.image = randomDog.image;
+//    self.breedLabel.text = randomDog.breed;
+//    self.nameLabel.text = randomDog.name;
+    [UIView transitionWithView:self.view duration:.5 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
+        self.myImageView.image = randomDog.image;
+        self.breedLabel.text = randomDog.breed;
+        self.nameLabel.text = randomDog.name;
+    } completion:^(BOOL finished) {
+        
+    }];
+    sender.title = @"And Another";
 }
 
 - (void) printHelloWorld
